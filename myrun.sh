09335c06c8c2c2d00e6ff52cbb09efff39233fbb
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2018-08-20 11:52:21 +0900

    2ndデータの利用

--- myrun.sh
+++ myrun.sh
@@ -3,6 +3,6 @@
 #--mode=train \
     
 python -m pdb run_summarization.py \
-       --data_path=s3://enc-report-data/pgn/train_data.bin \
-       --vocab_path=s3://enc-report-data/pgn/vocab.txt \
-       --log_root=../log-pgn --exp_name=mysum
+       --data_path=s3://enc-report-data/pgn/train_data-2nd.bin \
+       --vocab_path=s3://enc-report-data/pgn/vocab-2nd.txt \
+       --log_root=../log-pgn --exp_name=mysum-2nd
