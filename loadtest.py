9b9a4c4ad4a350f8dffdd7608148808fdea93725
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2018-08-20 17:02:40 +0900

    json化された記事データを読んでExampleインスタンス化する

--- loadtest.py
+++ loadtest.py
@@ -6,14 +6,20 @@ train data binary(pb2)を読み込む
 
 import argparse
 import collections
+import json
 from data import Vocab
 from batcher import Batcher
+import batcher as B
+
+import neologdn
+from natto import MeCab
 
 def get_args():
     p = argparse.ArgumentParser()
     p.add_argument('--vocab-size', type=int, default=50000)
     p.add_argument('--single-pass', default=False, action='store_true')
-    p.add_argument('data_path')
+    p.add_argument('--data_path', default="", type=str)
+    p.add_argument('--json-path', default="", type=str)
     p.add_argument('vocab_path')
     args = p.parse_args()
     return args
@@ -29,11 +35,23 @@ def main():
         'pointer_gen': True,
     }
     hps = collections.namedtuple('hps', _hps.keys())(**_hps)
-    batcher = Batcher(args.data_path, vocab, hps, args.single_pass)
-    import pdb; pdb.set_trace()
-    x = batcher.next_batch()
-    import pdb; pdb.set_trace()
-    pass
+    if not args.data_path == "":
+        batcher = Batcher(args.data_path, vocab, hps, args.single_pass)
+        import pdb; pdb.set_trace()
+        x = batcher.next_batch()
+        import pdb; pdb.set_trace()
+        pass
+    else:
+        with open(args.json_path) as f:
+            art = json.load(f)
+        article = neologdn.normalize(art['body'])
+        abstract = neologdn.normalize(art['title'])
+        m = MeCab('-Owakati')
+        parsed_article = m.parse(article)
+        abs_words = m.parse(abstract).split()
+        ex = B.Example(parsed_article, abs_words, vocab, hps)
+        import pdb; pdb.set_trace()
+        pass
 
 if __name__ == '__main__':
     main()
