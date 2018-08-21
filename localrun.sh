e00df1a2669074455dc73b520a478c7ac2feb253
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2018-08-21 17:00:05 +0900

    coverageの有効化

--- localrun.sh
+++ localrun.sh
@@ -5,4 +5,5 @@
 python -m pdb run_summarization.py \
        --data_path=$HOME/data/train_data-2nd.bin \
        --vocab_path=$HOME/data/vocab-2nd.txt \
-       --log_root=../log-pgn --exp_name=mysum-2nd
+       --coverage True \
+       --log_root=../log-pgn --exp_name=mysum-2nd-try2
