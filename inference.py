c11477365d4d2966fcd53ec6bd3136d8a8d4ede7
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2018-08-21 11:41:08 +0900

    decode処理の呼び出し、ログ出力

--- inference.py
+++ inference.py
@@ -26,9 +26,14 @@ class MyBatcher(B.Batcher):
         self._single_pass = single_pass
 
         self.batch = batch
+        self.count = 0
 
     def next_batch(self):
-        return batch
+        if self.count == 0:
+            self.count += 1
+            return self.batch
+        else:
+            return None
 
 def get_args():
     p = argparse.ArgumentParser()
@@ -48,6 +53,10 @@ tf.app.flags.DEFINE_string('json_path', '', '')
 tf.app.flags.DEFINE_boolean('pointer_gen', True, '')
 tf.app.flags.DEFINE_string('vocab_path', '', '')
 tf.app.flags.DEFINE_string('log_root', '', '')
+tf.app.flags.DEFINE_integer('beam_size', 1, '')
+tf.app.flags.DEFINE_integer('max_dec_steps', 100, 'max timesteps of decoder (max summary tokens)')
+tf.app.flags.DEFINE_integer('min_dec_steps', 35, 'Minimum sequence length of generated summary. Applies only for beam search decoding mode')
+tf.app.flags.DEFINE_boolean('coverage', False, 'Use coverage mechanism. Note, the experiments reported in the ACL paper train WITHOUT coverage until converged, and then train for a short phase WITH coverage afterwards. i.e. to reproduce the results in the ACL paper, turn this off for most of training then turn on for a short phase at the end.')
 
 def get_hps():
     _hps = {
@@ -83,6 +92,7 @@ def json_batch(fname, hps, vocab):
     return b
 
 def main():
+    tf.logging.set_verbosity(tf.logging.INFO) # choose what level of logging you want
     args = FLAGS # get_args()
     vocab = Vocab(args.vocab_path, args.vocab_size) # create a vocabulary
     hps = get_hps()
@@ -92,6 +102,7 @@ def main():
     decode_model_hps = hps._replace(max_dec_steps=1)
     model = SummarizationModel(decode_model_hps, vocab)
     decoder = BeamSearchDecoder(model, batcher, vocab)
+    decoder.decode()
     import pdb; pdb.set_trace()
     pass
 
